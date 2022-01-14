dec666e7ba4d4972f367a6eceda22105fea4d901
Tom L'Hermenier <tomlhermenier@gmail.com>
2022-01-14 08:45:44 +0100

    init des données initiales en static

--- java-restserver/src/main/java/infres/ws/Bdd.java
+++ java-restserver/src/main/java/infres/ws/Bdd.java
@@ -5,12 +5,14 @@ import infres.ws.rest.object.Place;
 import infres.ws.rest.object.Vol;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Random;
 
 public class Bdd {
 
-    private static ArrayList<Company> companies;
-    private static ArrayList<Vol> vols;
-    private static ArrayList<Place> places;
+    private ArrayList<Company> companies;
+    private ArrayList<Vol> vols;
+    private ArrayList<Place> places;
 
     private static Bdd instance;
 
@@ -18,13 +20,30 @@ public class Bdd {
         companies = new ArrayList<>();
         vols = new ArrayList<>();
         places = new ArrayList<>();
-        //TODO compléter les listes et les utiliser dans les différentes ressources
         for(int i = 0; i < 5; i++) {
-            companies.add(new Company("", i));
+            companies.add(new Company("company " + i, i));
+        }
+        for(int i = 0; i < 20; i++)  {
+            vols.add(new Vol(companies.get(i%5).getCompany(), i, new Date(), "Paris " + i, "Uzes " + i));
+            for(int j = 0; j < 3; j++) {
+                places.add(new Place(companies.get(i % 3).getCompany(), i, i * 3 + j, new Random().nextDouble(), true));
+            }
         }
     }
 
-    public Bdd getInstance() {
+    public ArrayList<Company> getCompanies() {
+        return companies;
+    }
+
+    public ArrayList<Vol> getVols() {
+        return vols;
+    }
+
+    public ArrayList<Place> getPlaces() {
+        return places;
+    }
+
+    public static Bdd getInstance() {
         if(instance == null) {
             instance = new Bdd();
         }
--- java-restserver/src/main/java/infres/ws/rest/ressource/RessourcePlace.java
+++ java-restserver/src/main/java/infres/ws/rest/ressource/RessourcePlace.java
@@ -1,5 +1,6 @@
 package infres.ws.rest.ressource;
 
+import infres.ws.Bdd;
 import infres.ws.rest.object.Place;
 
 import javax.ws.rs.*;
@@ -14,11 +15,7 @@ public class RessourcePlace {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Place> getAllPlace(@PathParam("company") String company, @PathParam("idVol") int idVol) {
-        ArrayList<Place> places = new ArrayList<>();
-        for(int i = 1; i < 21; i++) {
-            places.add(new Place(company, idVol, i, new Random().nextDouble() * 2000, new Random().nextBoolean()));
-        }
-        return places;
+        return Bdd.getInstance().getPlaces();
     }
 
     @GET
